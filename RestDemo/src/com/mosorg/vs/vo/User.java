27d8adf81d1092884ad8e42c1a3367ad6a5e102d
MyPC <mwb20088@163.com>
2019-03-10 17:36:34 +0800

    添加Rest示例源码 1、将war包部署到Tomcat 2、访问 http://localhost:8080/RestDemo/rest/restService/getUserText http://localhost:8080/RestDemo/rest/restService/getUserJson

---
+++ RestDemo/src/com/mosorg/vs/vo/User.java
@@ -0,0 +1,37 @@
+package com.mosorg.vs.vo;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class User {
+
+	private String name;
+
+	private String age;
+
+	private String sex;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+}
